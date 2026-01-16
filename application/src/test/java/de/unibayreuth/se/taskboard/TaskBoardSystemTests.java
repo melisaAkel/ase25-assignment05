1e42aee8bad073ed36aa382a896d42d666bf9b4e
melisa <irem.akel@ug.bilkent.edu.tr>
2026-01-16 22:18:34 +0100

    Tests

--- application/src/test/java/de/unibayreuth/se/taskboard/TaskBoardSystemTests.java
+++ application/src/test/java/de/unibayreuth/se/taskboard/TaskBoardSystemTests.java
@@ -4,14 +4,19 @@ import de.unibayreuth.se.taskboard.api.dtos.TaskDto;
 import de.unibayreuth.se.taskboard.api.mapper.TaskDtoMapper;
 import de.unibayreuth.se.taskboard.business.domain.Task;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.internal.http.HttpResponseException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.UUID;
 
+import de.unibayreuth.se.taskboard.business.ports.UserService;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
 
@@ -19,6 +24,9 @@ public class TaskBoardSystemTests extends AbstractSystemTest {
 
     @Autowired
     private TaskDtoMapper taskDtoMapper;
+    @Autowired
+    private UserService userService;
+
 
     @Test
     void getAllCreatedTasks() {
@@ -42,28 +50,68 @@ public class TaskBoardSystemTests extends AbstractSystemTest {
                 .containsExactlyInAnyOrderElementsOf(createdTasks);
     }
 
-    @Test
-    void createAndDeleteTask() {
-        Task createdTask = taskService.create(
-                TestFixtures.getTasks().getFirst()
-        );
+@Test
+void createAndDeleteTask() {
+    TaskDto createRequest = taskDtoMapper.fromBusiness(TestFixtures.getTasks().getFirst());
 
-        when()
-                .get("/api/tasks/{id}", createdTask.getId())
-                .then()
-                .statusCode(200);
+    TaskDto created = given()
+            .contentType(ContentType.JSON)
+            .body(createRequest)
+            .when()
+            .post("/api/tasks")
+            .then()
+            .statusCode(200)
+            .extract()
+            .as(TaskDto.class);
 
-        when()
-                .delete("/api/tasks/{id}", createdTask.getId())
-                .then()
-                .statusCode(200);
+    UUID id = created.getId();
 
-        when()
-                .get("/api/tasks/{id}", createdTask.getId())
-                .then()
-                .statusCode(400);
+    //there is
+    when()
+            .get("/api/tasks/{id}", id)
+            .then()
+            .statusCode(200);
 
+    // Delete
+    when()
+            .delete("/api/tasks/{id}", id)
+            .then()
+            .statusCode(200);
+
+    // After delete it gives BAD_REQUEST
+    try {
+        when().get("/api/tasks/{id}", id);
+        throw new AssertionError("Expected failure after delete, but request succeeded.");
+    } catch (Throwable e) {
+        assertThat(e).isInstanceOf(io.restassured.internal.http.HttpResponseException.class);
+        io.restassured.internal.http.HttpResponseException hre =
+                (io.restassured.internal.http.HttpResponseException) e;
+        assertThat(hre.getStatusCode()).isEqualTo(400);
     }
+}
 
-    //TODO: Add at least one test for each new endpoint in the users controller (the create endpoint can be tested as part of the other endpoints).
+
+
+    @Test
+    void getAllUsers() {
+        TestFixtures.createUsers(userService);
+        given()
+            .contentType(ContentType.JSON)
+            .when()
+            .get("/api/users")
+            .then()
+            .statusCode(200)
+            .body(".", hasSize(3));
+        }
+        @Test
+        void getUserById() {
+                var createdUsers = TestFixtures.createUsers(userService);
+                var user = createdUsers.getFirst();
+                when()
+                .get("/api/users/{id}", user.getId())
+                .then()
+                .statusCode(200);
+        }
+
+    //DODO: Add at least one test for each new endpoint in the users controller (the create endpoint can be tested as part of the other endpoints).
 }
