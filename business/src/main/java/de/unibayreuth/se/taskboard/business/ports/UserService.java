c065cf81c21b625fb19a7240fda25509e4fac513
melisa <irem.akel@ug.bilkent.edu.tr>
2026-01-16 21:43:16 +0100

    .

--- business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
+++ business/src/main/java/de/unibayreuth/se/taskboard/business/ports/UserService.java
@@ -1,6 +1,17 @@
 package de.unibayreuth.se.taskboard.business.ports;
+import de.unibayreuth.se.taskboard.business.domain.User;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface UserService {
-    //TODO: Add user service interface that the controller uses to interact with the business layer.
-    //TODO: Implement the user service interface in the business layer, using the existing user persistence service.
+    //DODO: Add user service interface that the controller uses to interact with the business layer.
+    //DODO: Implement the user service interface in the business layer, using the existing user persistence service.
+    List<User> getAllUsers();
+
+    User getUserById(UUID id);
+
+    User createUser(User user);
+
+    void clear();
 }
